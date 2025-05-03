ce7c4efb1bf29c14aaee93d096fd8d4ef1d36d79
akash <akashvishwakarma0602@gmail.com>
2025-05-04 03:57:32 +0530

    feat: add user registration route and controller with async error handling

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express"
 import cookieParser from "cookie-parser"
+import cors  from "cors";
 
 const app =  express()
 
@@ -9,6 +10,12 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended : true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+// routes
+import userRouter from "./routes/user.routes.js"
+
+// routes declearation
+app.use("/api/v1/users", userRouter)
 
 
 export { app }
