01ed5e4761f2e4d61de4468b7f236c3771849db1
akash <akashvishwakarma0602@gmail.com>
2025-05-06 18:52:36 +0530

    feat: configure CORS to allow requests from specified origin

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import cors  from "cors";
 
 const app =  express()
 
-app.use(cors())
+app.use(cors({origin: process.env.CORS_ORIGIN}))
 
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
